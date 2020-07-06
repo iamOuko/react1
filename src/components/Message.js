2be80342df9c6e87f0afb97a1de34d623a1b9aa1
ouko <oukoezzy@gmail.com>
2020-07-06 15:08:23 +0300

    first commit

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Message extends Component{
+    render() {
+        return (
+        <h1>
+            Welcome visitor
+            </h1>)
+    }
+}
+
+export default Message
